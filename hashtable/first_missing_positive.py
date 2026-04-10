a3960b26c714f3ac566c0d493a3431e9b2adfaae
dley18 <daneley18@gmail.com>
2026-04-09 23:06:12 -0400

    First Missing Positive

---
+++ hashtable/first_missing_positive.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        i = 0
+        while i < len(nums):
+            correct_index = nums[i] - 1
+            if 0 <= correct_index < len(nums) and nums[i] != nums[correct_index]:
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+            else:
+                i+=1
+
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+
+        return len(nums) + 1
+    
