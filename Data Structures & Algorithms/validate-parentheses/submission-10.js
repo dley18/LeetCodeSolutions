1b6661706d8fc87e278bbd1a298ae43b3066bac2
Dane Ley <123342361+dley18@users.noreply.github.com>
2026-05-06 15:11:31 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        const matching = {")": "(", "}": "{", "]": "["};
+
+        for (let i = 0; i < s.length; i++) {
+            if (Object.hasOwn(matching, s[i])) {
+                if (stack.length === 0 || stack[stack.length - 1] != matching[s[i]]) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(s[i]);
+            }
+        }
+
+        return stack.length > 0 ? false : true;
+    }
+}
