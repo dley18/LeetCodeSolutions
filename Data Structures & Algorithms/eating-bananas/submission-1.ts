1e2fd72fe3fa5a9fdffa55e10f33a60cbebd9d92
Dane Ley <123342361+dley18@users.noreply.github.com>
2026-05-29 14:53:56 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.ts
@@ -0,0 +1,27 @@
+class Solution {
+	/**
+	 * @param {number[]} piles
+	 * @param {number} h
+	 * @return {number}
+	 */
+	minEatingSpeed(piles: number[], h: number): number {
+		let l = 1;
+		let r = Math.max(...piles);
+		let res = r;
+
+		while (l <= r) {
+			let k = Math.trunc((l + r) / 2);
+			let hours = 0;
+			for (const p of piles) {
+				hours += Math.ceil(p / k);
+			}
+			if (hours <= h) {
+				res = Math.min(res, k);
+				r = k - 1;
+			} else {
+				l = k + 1;
+			}
+		}
+		return res;
+	}
+}
