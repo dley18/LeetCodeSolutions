f9be95bdbd2e8dfc793790ffcc9f802445d2fdf4
Dane Ley <123342361+dley18@users.noreply.github.com>
2026-06-01 15:10:21 -0400

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        let slow = head;
+        let fast = head.next;
+        while (fast && fast.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        let second = slow.next;
+        slow.next = null;
+        let prev = null;
+        while (second) {
+            let nextNode = second.next;
+            second.next = prev;
+            prev = second;
+            second = nextNode;
+        }
+
+        second = prev;
+        let first = head;
+        while (second) {
+            let tmp1 = first.next;
+            let tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
